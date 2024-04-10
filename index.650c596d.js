cace7322912c936746fde8cdbf2f6ab65570e40b
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-10 20:40:53 +0300

    make build

---
+++ index.650c596d.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");e.style.left="".concat((t.clientWidth-e.clientWidth)/2,"px"),e.style.top="".concat((t.clientWidth-e.clientWidth)/2,"px");
+//# sourceMappingURL=index.650c596d.js.map
